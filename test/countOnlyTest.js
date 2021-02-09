882d21cbb822c8125adbb938c89fc215f28167c3
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-09 03:14:45 +0000

    countOnly refactor

---
+++ test/countOnlyTest.js
@@ -0,0 +1,34 @@
+const assert = require('chai').assert;
+const countOnly = require('../countOnly');
+
+describe("#countOnly", () => {
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe",
+  ];
+  const result1 = countOnly(firstNames, {
+    Jason: true,
+    Karima: true,
+    Fang: true,
+    Agouhanna: false,
+  });
+  it(`return 1 for "Jason"`, () => {
+    assert.strictEqual(countOnly(firstNames, result1).Jason,1);
+  });
+  it(`return undefined for "Karima"`, () => {
+    assert.strictEqual(countOnly(firstNames, result1).Karima,undefined);
+  });
+  it(`return 2 for "Fang"`, () => {
+    assert.strictEqual(countOnly(firstNames, result1).Fang,2);
+  });
+  it(`return undefined for "Agouhanna"`, () => {
+    assert.strictEqual(countOnly(firstNames, result1).Agouhanna, undefined);
+  });
+});
