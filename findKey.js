e4dd236404c1aa13f8e885f1904fa3630730ad09
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-09 04:21:23 +0000

    refactor findKey

--- findKey.js
+++ findKey.js
@@ -1,22 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`💣 💣 💣 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`🎉 🎉 🎉 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-// start
 const findKey = (obj, callback) => Object.keys(obj).filter((item) => callback(obj[item]) && item)[0];
-
-// testing code
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
 
 module.exports = findKey;
