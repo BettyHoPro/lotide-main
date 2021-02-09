a77c868da1986106dccd2f97f5036209059e7638
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-09 03:42:25 +0000

    findKeyByValueTest.js refactor

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`💣 💣 💣 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`🎉 🎉 🎉 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-// start
-
 const findKeyByValue = (obj, val) => {
   for (let keyObj in obj) {
     if (obj[keyObj] === val) {
@@ -15,15 +5,5 @@ const findKeyByValue = (obj, val) => {
     }
   }
 };
-
-//test code
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
 module.exports = findKeyByValue;
