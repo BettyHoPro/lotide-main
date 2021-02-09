9dca76830faa38b3810eff1185d46733c3d14316
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-09 03:48:00 +0000

    refactor the eqArrays test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,20 @@
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays(["1", "2"], ["1", "2", 3]), false);
+describe("#eqArrays", () => {
+  it(`return  true for [1, 2, 3], [1, 2, 3]`, () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it(`return false for  [1, 2, 3], [3, 2, 1]`, () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it(`return true for ["1", "2", "3"], ["1", "2", "3"]"`, () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it(`return false for  ["1", "2", "3"], ["1", "2", 3]`, () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+  it(`return false for ["1", "2"], ["1", "2", 3]`, () => {
+    assert.strictEqual(eqArrays(["1", "2"], ["1", "2", 3]), false);
+  });
+});
